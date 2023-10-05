0d48cacaeb2ef793e861c3bfebf43dff779d6e8f
TheSarmon <shurikan189@gmail.com>
2023-10-06 01:28:41 +0300

    update

--- src/test.js
+++ src/test.js
@@ -1,68 +1,53 @@
 // ------------------------------------ PUBLIC ------------------------------------
 
+const { IS_F_MONOTONE , IS_F_LINEAR} = require("./is_monotone_or_linear");
+
+
 function CHECK_FAL_FOR_ALL(n1, n2) {
-  let constZero = false;
-  let constOne = false;
-
-  const str_n1 = (n1 >>> 0).toString(2).padStart(4, "0");
-  const str_n2 = (n2 >>> 0).toString(2).padStart(4, "0");
-
-  if (str_n1[0] === "0") {
-    constZero = true;
-  }
-
-  if (str_n2[3] === "1") {
-    constOne = true;
-  }
-
-  const str_x = str_n1.slice(1);
-  const str_y = str_n2.slice(0, 3);
-
-  const str_result = str_x + str_y;
-
-  // TODO: візьми функцію Віталі
-  let monotone =
-    str_result[0] <= str_result[1] &&
-    str_result[1] <= str_result[3] &&
-    str_result[3] <= str_result[7] &&
-    str_result[1] <= str_result[5] &&
-    str_result[5] <= str_result[7] &&
-    str_result[0] <= str_result[2] &&
-    str_result[2] <= str_result[3] &&
-    str_result[2] <= str_result[6] &&
-    str_result[6] <= str_result[7] &&
-    str_result[0] <= str_result[4] &&
-    str_result[4] <= str_result[5] &&
-    str_result[4] <= str_result[6];
-
-  // TODO: те ж саме, переюзни готову функцію, а не копіпасти код
-  let linear = true;
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      if (
-        parseInt(str_result[i]) ^
-        (parseInt(str_result[j]) !== parseInt(str_result[i ^ j]))
-      ) {
-        linear = false;
-        break;
-      }
+    let constZero = false;
+    let constOne = false;
+  
+    const str_n1 = (n1 >>> 0).toString(2).padStart(4, "0");
+    const str_n2 = (n2 >>> 0).toString(2).padStart(4, "0");
+  
+    if (str_n1[0] === "0") {
+      constZero = true;
     }
+  
+    if (str_n2[3] === "1") {
+      constOne = true;
+    }
+  
+    const str_x = str_n1.slice(1);
+    const str_y = str_n2.slice(0, 3);
+  
+    const str_result = str_x + str_y;
+  
+ 
+    let monotoneResult = IS_F_MONOTONE(str_result);
+    let linearResult = IS_F_LINEAR(str_result)
+    let selfDualityResult = _selfDuality(str_n1, str_n2);
+  
+    const result = `Function : const0 = ${constZero}, const1 = ${constOne}, Monotone: ${monotoneResult}, Linear: ${linearResult}, Selfduality : ${selfDualityResult}`;
+    console.log(result);
   }
 
-  // TODO: перепиши на англ
-  const result = `�������: �����0 = ${constZero}, �����1 = ${constOne}, ������������: ${monotone}, ˳��������: ${linear}`;
-  return result;
-}
+  if (typeof module !== "undefined") {
+    module.exports = {
+      IS_F_MONOTONE,
+      IS_F_LINEAR,
+      _selfDuality,
+      CHECK_FAL_FOR_ALL
+    };
+  }
+  // ------------------------------------ PRIVATE ------------------------------------
 
-// TODO: зроби експорт
-
-// ------------------------------------ PRIVATE ------------------------------------
-
-function _selfDuality(inputStr, compareStr) {
-  const invertedStr = inputStr
-    .split("")
-    .map((char) => (char === "0" ? "1" : "0"))
-    .reverse()
-    .join("");
-  return invertedStr === compareStr;
-}
+  function _selfDuality(inputStr, compareStr) {
+    const invertedStr = inputStr
+      .split("")
+      .map((char) => (char === "0" ? "1" : "0"))
+      .reverse()
+      .join("");
+    return invertedStr === compareStr;
+  }
+  CHECK_FAL_FOR_ALL(4,3)
