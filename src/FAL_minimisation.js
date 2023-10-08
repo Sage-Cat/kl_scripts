0fece5108034b376dfd1fda5250fc936638a9f9c
Field-Effect-Transistor <145497915+Field-Effect-Transistor@users.noreply.github.com>
2023-10-08 20:16:39 +0300

    Update FAL_minimisation.js

--- src/FAL_minimisation.js
+++ src/FAL_minimisation.js
@@ -22,10 +22,8 @@ function _fal_minimisation(fal, radix = 1)
 
     codes = BetterSort(codes, radix);
 
-    //console.log(codes);
-
-    let tmp = [];
-
+    let implicants = [];
+    let commonImplicants = [];
     //власне, мінімізація
     for(let i = 0; i < codes.length - 1; ++i)
     {
@@ -36,20 +34,21 @@ function _fal_minimisation(fal, radix = 1)
                 //console.log("k = " + k)
                 let position = dif(codes[i][j], codes[i + 1][k]);
                 let code = "";
-                if (position != -1) {
+                if (position != -1 && position != codes[i][j].length) {
                     code = codes[i][j];
-                    code = replaceAt(code, position, "-");
-                    tmp.push(code);
-                    console.log(code);
+                    implicants.push(replaceAt(code, position, "-"));
                 }
             }
         }
     }
 
-    return tmp;
+    return GetCommonImplicants(BetterSort(implicants, radix),implicants);
+
+
+
 }
 
-console.log(_fal_minimisation("FF"));
+console.log(_fal_minimisation("81"));
 //console.log(Counter("01010",1));
 
 /**
@@ -71,13 +70,16 @@ function Counter(code, radix)
 }
 
 /**
- *
+ *  якщо відмінність одна, то повертає позицію відмінності,
+ *  якщо більше, то -1,
+ *  якщо нема, то довжину рядка
  * @param {string}code1
  * @param {string}code2
  * @return {number}
  */
 function dif(code1, code2)
 {
+    //console.log(code1 + " " + code2)
     let counter = 0;
     let position = 0;
     for(let i = 0; i < code1.length; ++i)
@@ -85,12 +87,14 @@ function dif(code1, code2)
     {
         ++counter;
         position = i;
+        //console.log(code1[i] + " " + code2[i])
     }
 
     if(counter == 1)
         return position;
-    else
-        return -1;
+    if(counter == 0)
+        return code1.length
+    return -1;
 }
 
 /**
@@ -126,7 +130,10 @@ function replaceAt(str, pos, sym)
 function BetterSort(codes, radix)
 {
 
+    console.log(codes);
+
     let temp = [];
+    let tmp = [];
 
     for(let i = 0; i <= codes[0].length; ++i) {
         temp[i] = [];
@@ -138,10 +145,34 @@ function BetterSort(codes, radix)
     }
 
     //усунення порожніх підмасивів
-    for(let i = 0; i < temp.length; ++i)
-        if(temp[i].length == 0)
-            temp.splice(i, 1);
-    codes = temp.slice();
+    for(let i = 0; i < temp.length; ++i) {
+        //console.log(i + " " + temp[i]);
+        if (temp[i].length != 0)
+            tmp.push(temp[i]);
+    }
 
-    return temp;
+    return tmp;
 }
+
+/**
+ *
+ * @param {any[]}codes
+ * @param {string[]}implicants
+ * @return {string[]}
+ * @constructor
+ */
+    function GetCommonImplicants(codes, implicants)
+    {
+        let commonImplicants = [];
+        let tmp = [];
+
+        for(let i = 0; i < codes.length; ++i)
+            for(let j = 0; j < codes[i].length; ++j)
+                tmp.push(codes[i][j]);
+
+        for(let i = 0; i < tmp.length; ++i)
+            for(let j = 0; j < implicants.length; ++j)
+                if(dif(tmp[i], implicants[j]) != -1)
+                    commonImplicants.push(tmp[i]);
+        return commonImplicants;
+    }
