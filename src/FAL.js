1faf52895f543e979f827ca79ed046dcccf173af
Volodymyr <43709724+Sage-Cat@users.noreply.github.com>
2023-10-09 18:18:05 +0300

    test.js from txt (#5)
    * test.js from txt
    
    * update
    
    * Update test.js
    
    * Rename test.js to check_FAL.js
    
    * test.js from txt
    
    * update
    
    * Update test.js
    
    * Rename test.js to check_FAL.js
    
    * new changes
    
    * merging is_monotone_or_linear.js into check_FAL.js
    
    * remove other files,update check_FAL.js
    
    * Revert "remove other files,update check_FAL.js"
    
    This reverts commit 298e415ca40bf6f7df44aeb2dc5fa53b6982c2c3.
    
    * remove other files,update check_FAL.js
    
    ---------
    
    Co-authored-by: TheSarmon <shurikan189@gmail.com>
    Co-authored-by: TheSarmon <145500660+TheSarmon@users.noreply.github.com>

---
+++ src/FAL.js
@@ -0,0 +1,81 @@
+const { HEX2BIN_TETRAD } = require("./base_converts");
+
+function CHECK_FOR_FULLFUNC(n1, n2){
+    let constZero = false;
+    let constOne = false;
+  
+    const str_n1 = (n1 >>> 0).toString(2).padStart(4, "0");
+    const str_n2 = (n2 >>> 0).toString(2).padStart(4, "0");
+  
+    if (str_n1[0] === "0") {
+      constZero = true;
+    }
+  
+    if (str_n2[3] === "1") {
+      constOne = true;
+    }
+  
+    const str_x = str_n1.slice(1);
+    const str_y = str_n2.slice(0, 3);  
+    const str_result = str_x + str_y;
+
+    let monotoneResult = IS_F_MONOTONE(str_result);
+    let linearResult = IS_F_LINEAR(str_result)
+    let selfDualityResult = IS_F_SELFDUAL(str_n1, str_n2); 
+    const result = `Function : \nconst0:  ${constZero ? "Yes" : "No"}\nconst1:  ${constOne ? "Yes" : "No"}\nMonotone: ${monotoneResult ? "Yes" : "No"}\nLinear: ${linearResult ? "Yes" : "No"}\nSelfduality : ${selfDualityResult ? "Yes" : "No"}`;
+    return result;
+}
+
+  function IS_F_SELFDUAL(inputStr, compareStr){
+    const invertedStr = inputStr
+      .split("")
+      .map((char) => (char === "0" ? "1" : "0"))
+      .reverse()
+      .join("");
+    return invertedStr === compareStr;
+}
+
+  function IS_F_MONOTONE(data){
+    if(typeof(data != "string"))
+      data = "" + data;
+  let str = HEX2BIN_TETRAD(data);
+  let monotone = (
+      (str[0] <= str[1])
+      && (str[1] <= str[3])
+      && (str[3] <= str[7])
+      && (str[1] <= str[5])
+      && (str[5] <= str[7])
+      && (str[0] <= str[2])
+      && (str[2] <= str[3])
+      && (str[2] <= str[6])
+      && (str[6] <= str[7])
+      && (str[0] <= str[4])
+      && (str[4] <= str[5])
+      && (str[4] <= str[6])
+  );
+  return monotone;
+}
+
+function IS_F_LINEAR(data){
+  if(typeof(data != "string"))
+    data = "" + data;
+    let str = HEX2BIN_TETRAD(data);
+    let linear = true;
+    for(let i = 0; i <= 7; ++i)
+        for(let j = 0; j <= 7; ++j)
+            if(parseInt(str[i^j]) != (parseInt(str[i]) ^ parseInt(str[j])))
+            {
+                linear = false;
+                break;
+            }
+    return linear;
+}
+
+  if (typeof module !== "undefined"){
+    module.exports = {
+      IS_F_MONOTONE,
+      IS_F_LINEAR,
+      IS_F_SELFDUAL,
+      CHECK_FOR_FULLFUNC
+    };
+}
